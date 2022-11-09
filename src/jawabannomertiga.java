acc803fab243db6d98fdabf89fa8ac8011c7f120
xiaom <muttaqienshizame9@gmail.com>
2022-11-09 15:20:40 +0700

    oke

--- src/jawabannomertiga.java
+++ src/jawabannomertiga.java
@@ -2,5 +2,6 @@ public class jawabannomertiga {
     public static void main(String[] args) {
         System.out.println("hello class 1d");
         System.out.println("hggafsshfay");
+        System.out.println("shizame ganteng");
     }
 }
